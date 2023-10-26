ba10a1e9f08052e95dca006f6e9a54c01f9b6715
Arturo-Uc-Mz <114110633+Arturo-Uc@users.noreply.github.com>
2023-10-25 20:34:12 -0600

    Update Ejercicio_do_while.java

--- Arturo_Uc/Ejercicio_do_while.java
+++ Arturo_Uc/Ejercicio_do_while.java
@@ -1,26 +1,27 @@
+import java.util.ArrayList;
+
 public class Ejercicio_do_while {
 
     public static void main(String[] args) {
-        
-      int numeroNatural = 1;
+      ArrayList<Integer> numeros_impares = new ArrayList<>();  
+      int numero_natural = 1;
       int suma = 0;
       int conteo = 1;
       
       do {
-         if ((numeroNatural % 2)== 0){
-         
-           System.out.println(numeroNatural);
-           suma += numeroNatural;
-           numeroNatural ++;
-           conteo ++;
-         } else {
-             numeroNatural ++;
-         }
-         
+         if ((numero_natural % 2)== 0){
+            numero_natural ++;
 
+         } else {
+           suma += numero_natural;
+           numeros_impares.add(numero_natural);
+           numero_natural ++;
+           conteo ++;
+         }
         }
-      while (numeroNatural <= 20);
+      while (conteo <= 20);
       double promedio = suma/(conteo-1);
+      System.out.println("Los primeros 20 numeros naturales impares son: " + numeros_impares);
       System.out.println("La suma de los primeros 20 numeros naturales impares es de: " + suma);
       System.out.println("El promedio de los primeros 20 numeros naturales impares es de: " + promedio);
 
