a34416868884e0f8f7aff17e65c21149dbd779dc
ZeusZaragoza <148130845+ZeusZaragoza@users.noreply.github.com>
2023-10-26 20:38:24 -0600

    Add files via upload

---
+++ ZeusZaragoza/SumaMultiploDe5.java
@@ -0,0 +1,30 @@
+
+public class SumaMultiploDe5 {
+
+ 
+    
+  
+    public static void main(String[] args) {
+       
+        int suma = 0;
+
+       
+        int numero = 5;
+
+        
+        while (numero < 50) {
+            
+            suma += numero;
+
+            
+            numero += 5;
+        }
+
+        
+        System.out.println("La suma de los múltiplos de 5 menores a 50 es: " + suma);
+    }
+}
+
+
+    
+
