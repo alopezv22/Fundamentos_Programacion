f07de1b06a548ec8e7c0f914fbc948a7172cb728
DaniT0r0 <148005471+DaniT0r0@users.noreply.github.com>
2023-10-26 21:05:08 -0700

    Create numerosnaturales.java

---
+++ Daniel_Toro/Daniel_Toro_Actividad7/numerosnaturales.java
@@ -0,0 +1,28 @@
+package Daniel_Toro_Actividad_7;
+
+public class  numerosnaturales{
+    public static void main(String[] args) {
+
+        int num_suma = 1, suma = 0, cont_terminos = 0, numerador = 1;  // Comenzamos con el primer número impar
+
+        do {
+            System.out.println("__________________________________________________________________________");
+            System.out.println("|                                                                        |");
+            System.out.println("        ~~  El " + numerador + "° numero natural impar mennor a 20 es:  " + num_suma +"  ~~            ");
+            System.out.println("|________________________________________________________________________|");
+            suma = suma + num_suma;       
+            num_suma = num_suma + 2;               
+            cont_terminos++;            
+            numerador++;        
+        } while (cont_terminos < 20);
+
+        double promedio = (double) suma / cont_terminos;
+
+        System.out.println("__________________________________________________________________________");
+        System.out.println("|                                                                        |");
+        System.out.println("|         Promedio de los primeros 20 números naturales impares:         |" );
+        System.out.println(  "|                                   " + promedio + "                                 |");
+        System.out.println("|\"_______________________________________________________________________|");
+
+    }//Hecho por DaniT0r0
+}   //Favor de no copiar ;)
