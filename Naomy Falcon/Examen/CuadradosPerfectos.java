88ffdf70f55f6c34252cb46860d6000c06d94324
Naomy <148275964+Myan734@users.noreply.github.com>
2023-10-27 12:38:04 -0600

    Problema while

---
+++ Naomy Falcon/Examen/CuadradosPerfectos.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CuadradosPerfectos {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        int cuadrado;
+
+        System.out.println("Ingrese un número: ");
+        numero = sc.nextInt();
+
+        int i = 1;
+        while (i * i < numero) {
+            cuadrado = i * i;
+
+            System.out.println(cuadrado);
+            i++;
+        }
+    }
+}
