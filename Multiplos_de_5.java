ddbde6f4752abee3e204ace20b61f4b0fcaa5364
EduLC2005 <148163746+EduLC2005@users.noreply.github.com>
2023-10-27 18:20:33 -0600

    Add files via upload

---
+++ Multiplos_de_5.java
@@ -0,0 +1,19 @@
+
+package multiplos_de_5; 
+
+
+public class Multiplos_de_5 {
+     
+    public static void main(String[] args) {
+        int i = 0; 
+        int suma = 0; 
+while (i <= 50){
+    System.out.println(i); 
+    i+=5; 
+   suma += i; 
+   System.out.println("La suma de los múltiplos de 5 menores a 50 es: " + suma);
+    
+}
+}
+}
+        
